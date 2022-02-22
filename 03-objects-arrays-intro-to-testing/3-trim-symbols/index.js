6c768daf40855bead38f383a6e3a07ca3ed0d137
SerSurgut <sersurgut88@yandex.ru>
2022-02-23 02:53:05 +0300

    done 03-module

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,18 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
 
+export function trimSymbols(string, size) {
+  let _str = string[0];
+  const array = [];
+
+  for (let i = 1; i <= string.length; i++) {
+    if (string[i] === string[i - 1]) {
+      _str += string[i];
+    } else {
+      array.push(_str.slice(0, size));
+      _str = string[i];
+    }
+  }
+  return array.join("");
 }
